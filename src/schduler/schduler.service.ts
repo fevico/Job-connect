d28a1f11bcfe232a9a989a796854dca86318bf44
fevico <ajayifavour81@gmail.com>
2024-09-14 02:49:36 +0100

    update

--- src/schduler/schduler.service.ts
+++ src/schduler/schduler.service.ts
@@ -8,19 +8,19 @@ import { SubscriptionDocument } from 'src/subscription/schema/subcription.schema
 export class SchedulerService {
   constructor(private readonly subscriptionService: SubscriptionService) {}
 
-  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-  async handleCron() {
-    const now = new Date();
-    const subscriptions = await this.subscriptionService.findAllActive();
+//   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+//   // async handleCron() {
+//   //   const now = new Date();
+//   //   const subscriptions = await this.subscriptionService.findAllActive();
 
-    for (const subscription of subscriptions) {
-      if (moment(subscription.endDate).isBefore(now)) {
-        if (subscription.autoRenew) {
-          await this.subscriptionService.renewSubscription(subscription as SubscriptionDocument);
-        } else {
-          await this.subscriptionService.expireSubscription(subscription as SubscriptionDocument);
-        }
-      }
-    }
-  }
+//   //   for (const subscription of subscriptions) {
+//   //     if (moment(subscription.endDate).isBefore(now)) {
+//   //       if (subscription.autoRenew) {
+//   //         await this.subscriptionService.renewSubscription(subscription as SubscriptionDocument);
+//   //       } else {
+//   //         await this.subscriptionService.expireSubscription(subscription as SubscriptionDocument);
+//   //       }
+//   //     }
+//   //   }
+//   // }
 }
