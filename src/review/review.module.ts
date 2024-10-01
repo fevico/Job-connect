eaf26c67732988ef498ffaa3dad99fb77c302bdf
fevico <ajayifavour81@gmail.com>
2024-10-01 20:31:07 +0100

    changes

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,10 +3,14 @@ import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Review, ReviewSchema } from './schema/review.schema';
+import { Product, ProductSchema } from 'src/product/schema/product.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]),
+    MongooseModule.forFeature([
+      { name: Review.name, schema: ReviewSchema },
+      { name: Product.name, schema: ProductSchema }
+    ]),
   ],
   controllers: [ReviewController],
   providers: [ReviewService]
